5654804854ec4b1666e2a330eb06215bb2eefda1
Lewis Collard <lewiscollard@gmail.com>
2020-03-09 20:19:38 +0000

    Bump pytest version.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'testing': [
             'isort==4.3.21',
             'pylint==2.4.4',
-            'pytest>=3.6',
+            'pytest>=3.8',
             'pytest-cov',
             'pytest-django',
             'pytest-xdist',
