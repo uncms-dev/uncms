6447c3e61b884d3d6bab67fd9311265d3bbc4583
Lewis Collard <lewiscollard@gmail.com>
2022-10-14 23:17:38 +0100

    Simplify cms.templatetags.html tests.

--- cms/tests/test_templatetags_html.py
+++ cms/tests/test_templatetags_html.py
@@ -1,26 +1,26 @@
+import pytest
 from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
 
 from cms.apps.testing_models.models import HTMLModel
 from cms.templatetags.html import html, truncate_paragraphs
 
 
-class HTMLTest(TestCase):
+@pytest.mark.django_db
+def test_html():
+    assert html('') == ''
+    assert html(None) == 'None'
+    assert html('Hello') == 'Hello'
+    assert html('<span>Hello</span>') == '<span>Hello</span>'
 
-    def test_html(self):
-        self.assertEqual(html(''), '')
-        self.assertEqual(html(None), 'None')
-        self.assertEqual(html('Hello'), 'Hello')
-        self.assertEqual(html('<span>Hello</span>'), '<span>Hello</span>')
+    obj = HTMLModel.objects.create()
+    assert html('<a href="/r/{}-{}/">Hello</a>'.format(
+        ContentType.objects.get_for_model(HTMLModel).pk,
+        obj.pk
+    )) == '<a href="/foo/" title="Foo">Hello</a>'
 
-        obj = HTMLModel.objects.create()
-        self.assertEqual(html('<a href="/r/{}-{}/">Hello</a>'.format(
-            ContentType.objects.get_for_model(HTMLModel).pk,
-            obj.pk
-        )), '<a href="/foo/" title="Foo">Hello</a>')
 
-    def test_truncate_paragraphs(self):
-        self.assertEqual(truncate_paragraphs('<p>Foo', 1), '<p>Foo')
-        self.assertEqual(truncate_paragraphs('<p>Foo</p><p>Bar</p>', 0), '')
-        self.assertEqual(truncate_paragraphs('<p>Foo</p><p>Bar</p>', 1), '<p>Foo</p>')
-        self.assertEqual(truncate_paragraphs('<p>Foo</p><p>Bar</p>', 2), '<p>Foo</p><p>Bar</p>')
+def test_truncate_paragraphs():
+    assert truncate_paragraphs('<p>Foo', 1) == '<p>Foo'
+    assert truncate_paragraphs('<p>Foo</p><p>Bar</p>', 0) == ''
+    assert truncate_paragraphs('<p>Foo</p><p>Bar</p>', 1) == '<p>Foo</p>'
+    assert truncate_paragraphs('<p>Foo</p><p>Bar</p>', 2) == '<p>Foo</p><p>Bar</p>'
