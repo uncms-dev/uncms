ddfaa991503e5e8f51b03f438a4a3ca8d1ff7f68
Lewis Collard <lewiscollard@gmail.com>
2022-10-14 22:54:46 +0100

    Handle new WYSIWYG editors being created in inlines.

--- cms/static/cms/js/wysiwyg.js
+++ cms/static/cms/js/wysiwyg.js
@@ -5,6 +5,7 @@ window.uncms.activateEditor = function(element) {
 
     // Generate base settings for TinyMCE & merge with per-editor settings
     // (normally global settings)
+
     const settings = {
         selector: "#" + element.getAttribute("id")
     };
@@ -18,8 +19,8 @@ window.uncms.activateEditor = function(element) {
     window.tinymce.init(extendedSettings);
 };
 
-window.uncms.activateEditors = function() {
-    for (const element of document.querySelectorAll(".wysiwyg")) {
+window.uncms.activateEditors = function(parentElement) {
+    for (const element of (parentElement || document).querySelectorAll(".wysiwyg")) {
         // Don't activate one that is not visible.
         if (element.getBoundingClientRect().top) {
             window.uncms.activateEditor(element);
@@ -29,4 +30,14 @@ window.uncms.activateEditors = function() {
 
 window.addEventListener("load", function () {
     window.setTimeout(window.uncms.activateEditors, 100);
+
+    // Handle new editors being created in inlines.
+    //
+    // There is no way at all, as far as I can see, to handle the
+    // formset:added event without resorting to jQuery. :(
+    if (window.django && window.django.jQuery) {
+      window.django.jQuery(document).on("formset:added", (function(ev, jElement) {
+          window.uncms.activateEditors(jElement[0]);
+      }));
+    }
 });
