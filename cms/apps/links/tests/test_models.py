80c5d77299dee6ae421ba42b3ebf1b30316b422b
Lewis Collard <lewiscollard@gmail.com>
2022-10-15 19:41:13 +0100

    Factory-ise link model tests. [#16]

--- cms/apps/links/tests/test_models.py
+++ cms/apps/links/tests/test_models.py
@@ -1,22 +1,13 @@
 import pytest
-from django.contrib.contenttypes.models import ContentType
-from watson.search import update_index
 
 from cms.apps.links.models import Link
-from cms.apps.pages.models import Page
+from cms.apps.pages.tests.factories import PageFactory
 
 
 @pytest.mark.django_db
 def test_link_str():
-    with update_index():
-        page = Page.objects.create(
-            title='Homepage',
-            content_type=ContentType.objects.get_for_model(Link),
-        )
-
-        link = Link.objects.create(
-            page=page,
-            link_url='http://www.example.com/',
-        )
-
-    assert str(link) == 'Homepage'
+    link = PageFactory(
+        title='Link test',
+        content=Link(link_url='http://www.example.com/'),
+    )
+    assert str(link.content) == 'Link test'
