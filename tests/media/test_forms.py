f9afede8eb6d4c80118e8836183d21b91a78de44
Lewis Collard <lewiscollard@gmail.com>
2022-12-03 20:06:20 +0000

    [#17] Add test coverage for ImageChangeForm.

--- tests/media/test_forms.py
+++ tests/media/test_forms.py
@@ -1,7 +1,15 @@
+import base64
+
+import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from tests.media.factories import MINIMAL_GIF_DATA
-from uncms.media.forms import FileForm
+from tests.media.factories import (
+    MINIMAL_GIF_DATA,
+    SampleJPEGFileFactory,
+    SamplePNGFileFactory,
+    data_file_path,
+)
+from uncms.media.forms import FileForm, ImageChangeForm
 
 
 def test_fileform_validation():
@@ -39,3 +47,45 @@ def test_fileform_validation():
             'file': SimpleUploadedFile(name=f'sample.{extension}', content=MINIMAL_GIF_DATA, content_type=mimetype)
         })
         assert_has_file_mismatch(form)
+
+
+@pytest.mark.django_db
+# Test both the "it's a JPEG" branch and the implied "not a JPEG" branches.
+@pytest.mark.parametrize('factory', [SampleJPEGFileFactory, SamplePNGFileFactory])
+def test_filechangeform_save(factory):
+    original = factory()
+    assert original.width == 1920
+    with open(data_file_path('800x600.png'), 'rb') as fd:
+        changed_data = ''.join([';base64,', base64.b64encode(fd.read()).decode('utf-8')])
+    form = ImageChangeForm(
+        instance=original,
+        data={
+            'title': original.title,
+            'changed_image': changed_data,
+        },
+    )
+    assert form.is_valid()
+    form.save()
+
+    original.refresh_from_db()
+    assert original.width == 800
+    assert original.height == 600
+
+
+@pytest.mark.django_db
+def test_filechangeform_save_no_changes_branch():
+    """
+    Test the "changed_data is not present" branch.
+    """
+    original = SamplePNGFileFactory()
+    assert original.width == 1920
+    form = ImageChangeForm(
+        instance=original,
+        data={
+            'title': original.title,
+            'changed_image': '',
+        },
+    )
+    assert form.is_valid()
+    original.refresh_from_db()
+    assert original.width == 1920
