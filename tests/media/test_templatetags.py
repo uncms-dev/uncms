8cf4bbb643351c880b54b5a85e9361daeedf4bb0
Lewis Collard <lewiscollard@gmail.com>
2022-12-03 04:13:29 +0000

    [#17] Add a test for render_image template tag.
    While we're there, remove a TestCase and convert it into a test function
    (cf. #22). Also, make it not a completely so;;y test - test it on an actual
    file.

--- tests/media/test_templatetags.py
+++ tests/media/test_templatetags.py
@@ -1,11 +1,30 @@
-from django.test import TestCase
+import pytest
+from bs4 import BeautifulSoup
 from sorl.thumbnail.images import ImageFile
 
-from uncms.media.templatetags.media import thumbnail
+from tests.media.factories import MinimalGIFFileFactory, SamplePNGFileFactory
+from uncms.media.templatetags.media import render_image, thumbnail
 
 
-class TestTemplatetags(TestCase):
+@pytest.mark.django_db
+def test_thumbnail():
+    file = MinimalGIFFileFactory()
+    thumb = thumbnail(file.file, '100')
+    assert isinstance(thumb, ImageFile)
 
-    def test_thumbnail(self):
-        thumb = thumbnail('foo', '100')
-        self.assertIsInstance(thumb, ImageFile)
+
+@pytest.mark.django_db
+def test_render_image(client):
+    # Deeper tests will be in File.render_multi_format - just do a basic test
+    # to ensure it's outputting something that looks like HTML with a
+    # non-broken image
+    file = SamplePNGFileFactory()
+    html = render_image(file, width=600, height=400)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    url = soup.find('source')['srcset'].split()[0]
+    response = client.get(url)
+    assert response.status_code == 302
+
+    response = client.get(response['Location'])
+    assert response.status_code == 200
