3824aa46dac4aa56e7ef3bb57b22b5172f308c85
Lewis Collard <lewiscollard@gmail.com>
2022-12-20 16:13:34 +0000

    Remove un-needed thumbnail function.
    It was originally added as a Jinja2 helper for Sorl. If it's needed
    project-locally, it is trivial to add it. But people probably want
    to use `{% image %}` instead.

--- tests/media/test_templatetags.py
+++ tests/media/test_templatetags.py
@@ -1,25 +1,17 @@
 import pytest
 from bs4 import BeautifulSoup
-from sorl.thumbnail.images import ImageFile
 
-from tests.media.factories import MinimalGIFFileFactory, SamplePNGFileFactory
-from uncms.media.templatetags.media import render_image, thumbnail
-
-
-@pytest.mark.django_db
-def test_thumbnail():
-    file = MinimalGIFFileFactory()
-    thumb = thumbnail(file.file, '100')
-    assert isinstance(thumb, ImageFile)
+from tests.media.factories import SamplePNGFileFactory
+from uncms.media.templatetags.uncms_images import image
 
 
 @pytest.mark.django_db
-def test_render_image(client):
+def test_image(client):
     # Deeper tests will be in File.render_multi_format - just do a basic test
     # to ensure it's outputting something that looks like HTML with a
     # non-broken image
     file = SamplePNGFileFactory()
-    html = render_image(file, width=600, height=400)
+    html = image(file, width=600, height=400)
     soup = BeautifulSoup(html, 'html.parser')
 
     url = soup.find('source')['srcset'].split()[0]
