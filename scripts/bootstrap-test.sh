1d498e258f0c3a3a05d74c154f18786fe913fee5
Lewis Collard <lewiscollard@gmail.com>
2025-12-19 01:23:02 +0000

    Add some options to the bootstrap script.

--- scripts/bootstrap-test.sh
+++ scripts/bootstrap-test.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 set -eu
+
+NO_FRONTEND=false
+PYTHON_VERSION="python3"
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --no-frontend)
+            NO_FRONTEND=true
+            shift
+            ;;
+        --python)
+            PYTHON_VERSION="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
 git config blame.ignoreRevsFile .git-blame-ignore-revs
 rm -rf .venv
-python3 -m venv .venv
+$PYTHON_VERSION -m venv .venv
 . .venv/bin/activate
 pip install --upgrade pip
 # silence a warning, i don't like yellow on my console
 pip install wheel
 pip install -e .[dev]
 
-. ~/.nvm/nvm.sh
-nvm install
-npm install
+if [ "$NO_FRONTEND" = false ]; then
+    . ~/.nvm/nvm.sh
+    nvm install
+    npm install
+fi
